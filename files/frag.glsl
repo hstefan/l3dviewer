db31463ee1624da7f48dd369cb2cdb682e670415
Hugo Puhlmann <hugopuhlmann@gmail.com>
2017-01-01 17:07:56 -0200

    Experiment: texture blending with wavy senoidal effect on mirrored tex.

--- files/frag.glsl
+++ files/frag.glsl
@@ -1,17 +1,32 @@
 #version 150
 
-in float t;
-in vec3 fragColor;
-in vec2 texCoord_;
+in vec4 FragColor;
+in vec2 TexCoord;
 
 out vec4 outColor;
 
+uniform float timeSinceStart;
 uniform sampler2D texPepper;
 uniform sampler2D texBacon;
 
 void main() {
-  vec3 invColor = vec3(1, 1, 1) - fragColor;
-  vec4 colorPepper = texture(texPepper, texCoord_);
-  vec4 colorBacon = texture(texBacon, texCoord_);
-  outColor = mix(colorPepper, colorBacon, t) * vec4(t * invColor, 1.0);
+  float t = (sin(timeSinceStart * 4.0) + 1.0) * 0.5;
+
+  // recalculate coords to generate a mirrored wavy tex in the bottom half
+  vec2 coord = TexCoord;
+  if (coord.y > 0.5) {
+    coord.y = 0.5 - (coord.y - 0.5);
+    coord.x = coord.x + (sin(coord.y * 60 + timeSinceStart * 2.0) / 30.0);
+  }
+
+  // sample color from both textures
+  vec4 colorPepper = texture(texPepper, coord);
+  vec4 colorBacon = texture(texBacon, coord);
+
+  // mixes both textures along the animation time
+  vec4 mixedColor = mix(colorPepper, colorBacon, t);
+
+  // applies tinted color to output color
+  vec4 tintedColor = mixedColor * FragColor;
+  outColor = mix(mixedColor, tintedColor, 0.6);
 };
--- files/vertex.glsl
+++ files/vertex.glsl
@@ -1,18 +1,14 @@
 #version 150
 
-uniform float timeSinceStart;
-
 in vec2 position;
 in vec3 color;
 in vec2 texCoord;
 
-out float t;
-out vec3 fragColor;
-out vec2 texCoord_;
+out vec4 FragColor;
+out vec2 TexCoord;
 
 void main() {
-  t = (sin(timeSinceStart * 4.0) + 1.0) * 0.5;
-  fragColor = color;
+  FragColor = vec4(color, 1.0);
   gl_Position = vec4(position.x, position.y, 0.0, 1.0);
-  texCoord_ = texCoord;
+  TexCoord = texCoord;
 };
